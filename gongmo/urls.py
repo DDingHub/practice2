de382576f9fe297cbea2a26bb9e6ed5da66f7f92
haejun1 <a01027833227@gmail.com>
2023-07-27 14:36:47 +0900

    delete : drfproject삭제

--- gongmo/urls.py
+++ gongmo/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'gongmo'
 
 urlpatterns = [
-    path('api/contest-list/', views.ContestListAPIView.as_view(), name='contest_list_api'),
+    path('contest-list/', views.ContestListAPIView.as_view(), name='contest_list'),
 ]
