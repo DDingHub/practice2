a4e19edded3914bfb1f5b5acb95ef041a6780cf5
seoyeon chae <83562035+che-so@users.noreply.github.com>
2023-08-22 08:02:16 +0900

    미완성 해준 완성 바람

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -4,4 +4,4 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['user', 'jickgoon_type', 'hobbies', 'courses', 'workingTimes', 'characters', 'mbti',]
+        fields = ['user', 'name', 'jickgoon_type', 'hobbies', 'courses', 'workingTimes', 'characters', 'mbti',]
--- mypage/views.py
+++ mypage/views.py
@@ -6,11 +6,11 @@ from django.contrib.auth.models import User
 
 class UserMyPageView(APIView):
     def post(self, request, *args, **kwargs):
-        username = request.data.get('username')  # POST 데이터에서 username을 가져옴
+        email = request.data.get('email')  # POST 데이터에서 이메일을 가져옴
 
         try:
-            user = User.objects.get(username=username)  # username에 해당하는 유저 가져옴
-            user_profile = UserProfile.objects.get(user=user)  # 해당 유저에 대한 프로필 정보 가져옴
+            user = User.objects.get(email=email)  # 이메일에 해당하는 유저를 가져옴
+            user_profile = UserProfile.objects.get(user=user)  # 유저에 해당하는 프로필을 가져옴
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
         except UserProfile.DoesNotExist:
@@ -20,12 +20,23 @@ class UserMyPageView(APIView):
         response_data = {
             "username": user_profile.user.username,
             "email": user_profile.user.email,
-            "name": user_profile.name,
-            # 다른 필드들도 필요한 만큼 추가
+            "nickname": user_profile.nickname,
+            "major": user_profile.major,
+            "job": user_profile.job,
+            "hobby": user_profile.hobby,
+            "dream": user_profile.dream,
+            "tendency_worktime": user_profile.tendency_worktime,
+            "tendency_personality": user_profile.tendency_personality,
+            "tendency_MBTI": user_profile.tendency_MBTI,
+            "languages_tools": user_profile.languages_tools,
+            "call": user_profile.call,
+            "introduce": user_profile.introduce,
+            "portfolio": user_profile.portfolio,
+            "user_type": user_profile.user_type,
+            "type_message": user_profile.type_message,
         }
 
-        return Response(response_data, status=status.HTTP_200_OK)
-    
+        return Response(response_data, status=status.HTTP_200_OK)    
 class UserProfileJsonView(APIView):
     def post(self, request, *args, **kwargs):
         user_profile = UserProfile.objects.filter(user=request.user).first()
