cfc97b2ec40a9ccc667403388c6af9c5c3ec57d9
haejun1 <a01027833227@gmail.com>
2023-08-23 04:59:48 +0900

    유형테스트 - 이미 했던 값이 있으면 지우고 새것으로 업데이트 해줌

--- character/views.py
+++ character/views.py
@@ -9,11 +9,6 @@ from rest_framework import status
 
 #유형테스트 목록, 유형테스트 등록(db삭제될까봐 만듦)
 class CharacterAPIView(APIView):
-    # def get(self,request):
-    #     type_id = request.data.get('typeId')
-    #     characters = Character.objects.filter(id=type_id)
-    #     serializer = CharacterSerializer(characters, many=True)
-    #     return Response(serializer.data)
     def post(self, request):
         type_id = request.data.get('typeID')
         print(type_id)
@@ -46,11 +41,13 @@ class MyCharacterAPIView(APIView):
 
         if character_id:
             character = get_object_or_404(Character, pk=character_id)
-            my_character, created = MyCharacter.objects.get_or_create(user=user, character=character)
+            defaults = {"user": user, "character": character}
+            MyCharacter.objects.filter(user=user).delete()
+            my_character, created = MyCharacter.objects.update_or_create(user=user, character=character, defaults=defaults)
 
             if created:
-                return Response({"message": "Character assigned successfully."}, status=status.HTTP_201_CREATED)
+              return Response({"message": "Character assigned successfully."}, status=status.HTTP_201_CREATED)
             else:
-                return Response({"message": "Character already assigned to the user."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "Character assignment updated."}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "character_id is required."}, status=status.HTTP_400_BAD_REQUEST)
